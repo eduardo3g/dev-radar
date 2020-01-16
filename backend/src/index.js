80f896bc86fae8f191375cbe729a92e65a59a264
= <=>
2020-01-15 22:45:49 -0300

    chore: Added cors on backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ns1bp.mongodb.net/w
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
