d48c778553ab526fd73e6e2da2f77de623077406
= <=>
2020-01-15 22:30:29 -0300

    web: chore > Responsive design.

--- web/src/App.js
+++ web/src/App.js
@@ -11,23 +11,23 @@ function App() {
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <div className="input-block">
+          <div class="input-block">
             <label htmlFor="github_username">Usuário</label>
             <input name="github_username" id="github_username" required />
           </div>
 
-          <div className="input-block">
+          <div class="input-block">
             <label htmlFor="techs">Tecnologias</label>
             <input name="techs" id="techs" required />
           </div>
 
           <div className="input-group">
-            <div className="input-block">
+            <div class="input-block">
               <label htmlFor="latitude">Latitude</label>
               <input name="latitude" id="latitude" required />
             </div>
 
-            <div className="input-block">
+            <div class="input-block">
               <label htmlFor="longitude">Longitude</label>
               <input name="longitude" id="longitude" required />
             </div>
