9530a77b73877c085604d6b9486e0dd624bea704
= <=>
2020-01-17 17:57:33 -0300

    mobile: chore > Searching devs by inputing one or more technologies on text input.

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -9,7 +9,8 @@ import api from '../services/api';
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null)
-
+  const [techs, setTechs] = useState('');
+  
   useEffect(() => {
     async function loadInitialPosition() {
       const { granted } = await requestPermissionsAsync();
@@ -40,7 +41,7 @@ function Main({ navigation }) {
       params: {
         latitude,
         longitude,
-        techs: 'ReactJS',
+        techs,
       },
     });
 
@@ -92,6 +93,8 @@ function Main({ navigation }) {
             placeholderTextColor="#999"
             autoCapitalize="words"
             autoCorrect={false}
+            value={techs}
+            onChangeText={setTechs}
           />
 
           <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
