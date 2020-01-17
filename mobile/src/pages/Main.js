f41a72fad944c69bcdb928cea02ee1b65be944bb
= <=>
2020-01-17 14:46:56 -0300

    mobile: chore > MapView initial configuration.

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-// import { Container } from './styles';
+function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null)
 
-export default function Main() {
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if(!currentRegion) {
+    return null;
+  }
+
   return (
-    <View />
+    <MapView initialRegion={currentRegion} style={styles.map} />
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  }
+});
+
+export default Main;
