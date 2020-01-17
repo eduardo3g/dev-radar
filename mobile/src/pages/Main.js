feb3d13075798acde72508ddb9c5e8a19b49dad8
= <=>
2020-01-17 19:45:38 -0300

    mobile: chore > Connecting Main page with backend Socket.

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect } from '../services/socket';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -34,6 +35,16 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  function setupWebSocket() {
+    const { latitude, longitude } = currentRegion;
+
+    connect(
+      latitude,
+      longitude,
+      techs
+    );
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -46,6 +57,7 @@ function Main({ navigation }) {
     });
 
     setDevs(response.data.devs);
+    setupWebSocket();
   }
 
   function handleRegionChanged(region) {
