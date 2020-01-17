bf0d1517b6c71e3ced33ee79479c91506d48f7ce
= <=>
2020-01-17 19:44:20 -0300

    mobile: chore > Sending parameters (latitude, longitude and techs) to backend via Socket.

---
+++ mobile/src/services/socket.js
@@ -0,0 +1,26 @@
+import socketio from 'socket.io-client';
+
+const socket = socketio('http://192.168.0.9:3333', {
+  autoConnect: false,
+});
+
+function connect(latitude, longitude, techs) {
+  socket.io.opts.query = {
+    latitude,
+    longitude,
+    techs,
+  };
+
+  socket.connect();
+}
+
+function disconnect() {
+  if (socket.connected) {
+    socket.disconnect();
+  }
+}
+
+export {
+  connect,
+  disconnect
+}
