056b38d55e812c55613fd55b4c5b278697e0a308
= <=>
2020-01-17 20:50:17 -0300

    mobile: chore > Receiving backend data and placing dev info on MapView in real time.

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -4,6 +4,10 @@ const socket = socketio('http://192.168.0.9:3333', {
   autoConnect: false,
 });
 
+function subscribeToNewDevs(subscribeFunction) {
+  socket.on('new-dev', subscribeFunction);
+}
+
 function connect(latitude, longitude, techs) {
   socket.io.opts.query = {
     latitude,
@@ -22,5 +26,6 @@ function disconnect() {
 
 export {
   connect,
-  disconnect
+  disconnect,
+  subscribeToNewDevs
 }
