cd5d2f7609b5b2c96b2a96cea243eb48a728e27a
= <=>
2020-01-17 15:19:13 -0300

    mobile: chore > WebView configuration.

--- mobile/src/pages/Profile.js
+++ mobile/src/pages/Profile.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-// import { Container } from './styles';
+function Profile({ navigation }) {
+  const githubUserName = navigation.getParam('github_username');
 
-export default function Profile() {
   return (
-    <View />
+    <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${githubUserName}` }} />
   );
 }
+
+export default Profile;
