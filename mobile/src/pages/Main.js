5c6b62f00d18c7df813c9594e1997469e6662b95
jraoult <j.raoult@gmail.com>
2022-02-06 17:18:41 +0100

    + add radar animation

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard, Dimensions } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -7,11 +7,18 @@ import { MaterialIcons } from '@expo/vector-icons';
 import api from '../services/api';
 import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
+import mapStyle from './mapStyle.json';
+
+const Pulse = require('react-native-pulse').default;
+
+const {height, width} = Dimensions.get('window');
+
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
   const [currentRegion, setCurrentRegion] = useState(null)
   const [techs, setTechs] = useState('');
-  
+  const [warning, setWarning] = useState('False');
+
   useEffect(() => {
       async function loadInitialPosition() {
 
@@ -67,6 +74,7 @@ function Main({ navigation }) {
 
     setDevs(response.data.devs);
     setupWebSocket();
+    setWarning(!warning)
   }
 
   function handleRegionChanged(region) {
@@ -78,13 +86,19 @@ function Main({ navigation }) {
     return null;
   }
 
-  return (
-    <>
-    <MapView
-      onRegionChangeComplete={handleRegionChanged}
-      onPress={() => Keyboard.dismiss()}
-      initialRegion={currentRegion}
-      style={styles.map}>
+    let search = `Search... ${warning}`
+
+    // console.log("rerender")
+    
+    return (
+         <View style={ styles.container }>
+	    <MapView
+                onRegionChangeComplete={handleRegionChanged}
+                onPress={() => Keyboard.dismiss()}
+	        initialRegion={currentRegion}
+	        style={styles.map}
+	        customMapStyle={mapStyle}
+            >
         {devs.map(dev => (
           <Marker
           key={dev._id}
@@ -93,9 +107,8 @@ function Main({ navigation }) {
               latitude: dev.location.coordinates[1],              
             }}
           >
-            <Image style={styles.avatar} source={{ uri: dev.avatar_url }} />
-
-            <Callout onPress={() => {
+            <Image style={styles.avatar} source={{ uri: dev.avatar_url }} /> 
+              {           <Callout onPress={() => {
               navigation.navigate('Profile', { github_username: dev.github_username });
             }}>
               <View style={styles.callout}>
@@ -103,33 +116,58 @@ function Main({ navigation }) {
                 <Text style={styles.devBio}>{dev.bio}</Text>
                 <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
               </View>
-            </Callout>
+              </Callout> }
           </Marker>
         ))}
       </MapView>
       <View style={styles.searchForm}>
           <TextInput
-            style={styles.searchInput} 
-            placeholder="search..."
-            placeholderTextColor="#999"
-            autoCapitalize="words"
-            autoCorrect={false}
-            value={techs}
-            onChangeText={setTechs}
+              style={styles.searchInput} 
+              placeholder={search}
+              placeholderTextColor="#999"
+              autoCapitalize="words"
+              autoCorrect={false}
+              value={techs}
+              onChangeText={setTechs}
           />
 
           <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
             <MaterialIcons name="my-location" size={20} color="#FFF" />
           </TouchableOpacity>
       </View>
-    </>
+            {warning && 
+             <View style={styles.pulse}>
+                 <Pulse color='red' numPulses={3} diameter={500} speed={10} duration={2000} />
+             </View> }
+            {warning && 
+             <View style={styles.info}>
+                 <Text style={{color: 'white', fontSize: 40, fontWeight: 'bold'}}>You are close </Text>
+                 <Text style={{color: 'white', fontSize: 40, fontWeight: 'bold'}}>to the interactive </Text>
+                 <Text style={{color: 'white', fontSize: 40, fontWeight: 'bold'}}>point</Text>
+             </View> }
+            {!warning && 
+             <View style={styles.pulse}>
+                 <Pulse color='white' numPulses={3} diameter={500} speed={10} duration={2000} />
+             </View> }
+         </View>             
   );
 }
 
 const styles = StyleSheet.create({
-  map: {
-    flex: 1
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0)',        
+    },
+    
+    map: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
   
   avatar: {
     width: 54,
@@ -194,6 +232,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginLeft: 15,
   },
+
+  pulse: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      top: height/2+80,
+      zIndex: 0,
+  },
+    
+  info: {
+      top: -160,
+      alignItems: 'center',
+      justifyContent: 'center',
+      zIndex: 0,
+  },
+    
 });
 
 export default Main;
